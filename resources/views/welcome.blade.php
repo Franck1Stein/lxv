50849d6422e807a43cff869ca4efa7e8f3c151c1
franck1Stein <franck.adokou@gmail.com>
2021-06-15 07:40:23 +0000

    first trial!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Steincorp
                 </div>
 
                 <div class="links">
